808a09ae90f1e27aac4c7849be693fa50f63315b
makinde timilehin <makindetimi@gmail.com>
2022-04-10 19:09:52 +0100

    updates

--- src/components/HomeNavbar/index.tsx
+++ src/components/HomeNavbar/index.tsx
@@ -8,10 +8,10 @@ function HomeNavbar() {
         <div className={styles.main}>
             <img src="/poket-logo.png" style={{ width: '110px', height: '30px' }} alt="logo" />
             <div className={styles.nav__items}>
-                <span>Join early access</span>
-                <span>
+                <a href="https://poketfi.money#form" style={{ color: 'black', textDecoration: 'none' }}>Join early access</a>
+                {/* <span>
                   <Button text="LOG IN" bgColor='#00AFF5' width='174px' height='52px' textColor='white' />
-                </span>
+                </span> */}
             </div>
         </div>
     </div>
