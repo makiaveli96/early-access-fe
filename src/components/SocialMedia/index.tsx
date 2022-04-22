c9656b6365778364a71b28ac1e1c884ba3adc037
makinde timilehin <makindetimi@gmail.com>
2022-04-22 10:23:22 +0100

    added 'open new tab to social media links'

--- src/components/SocialMedia/index.tsx
+++ src/components/SocialMedia/index.tsx
@@ -15,6 +15,7 @@ function SocialMedia({ style, margin, type, refMessage }: { style?: any; margin?
         <SocialIcon
           style={style}
           network="facebook"
+          target="_blank"
           url={type == 'referral'? `https://www.facebook.com/sharer.php?u=https://poketfi.money?ref%3D${userDetails?.referralID}` : "https://www.facebook.com/poketfi"}
         />
       </span>
@@ -22,6 +23,7 @@ function SocialMedia({ style, margin, type, refMessage }: { style?: any; margin?
         <SocialIcon
           style={style}
           network="linkedin"
+          target="_blank"
           url={type == 'referral'? `https://www.linkedin.com/sharing/share-offsite/?url=https://poketfi.money?ref%3D${userDetails?.referralID}` : "https://www.linkedin.com/company/78340869/admin/"}
         />
       </span>
@@ -29,6 +31,7 @@ function SocialMedia({ style, margin, type, refMessage }: { style?: any; margin?
         <SocialIcon
           style={style}
           network="twitter"
+          target="_blank"
           url={type == 'referral'? `https://twitter.com/intent/tweet?text=${refMessage}` : "https://twitter.com/poketfinance"}
         />
       </span>
@@ -37,6 +40,7 @@ function SocialMedia({ style, margin, type, refMessage }: { style?: any; margin?
           <SocialIcon
             style={style}
             network="instagram"
+            target="_blank"
             url="https://www.instagram.com/poketfinance/"
           />
         </span>
